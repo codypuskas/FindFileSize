4e684e12852cf63e969065a2e6122d086bb7fdaa
Cody Puskas <codypuskas@gmail.com>
2017-05-01 15:36:27 -0400

    Add files via upload

---
+++ FindFileSize/FindFileSize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FindFileSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Drag and drop a file to get the size: ");
+            string path = Console.ReadLine();
+            FileInfo file = new FileInfo(path);
+            var size = file.Length;
+            Console.WriteLine("File size in Bytes: {0} bytes", size);
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
